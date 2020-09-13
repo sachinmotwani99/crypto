7f8280986dc858f794e59f52ebe407b71ec04985
sachinmotwani99 <63347647+sachinmotwani99@users.noreply.github.com>
2020-09-13 10:51:28 +0530

    Addes the tools for javascript

---
+++ tool.js
@@ -0,0 +1,31 @@
+var table = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21
+              ,'w':22,'x':23,'y':24,'z':25};
+function gcd(a,b)
+{
+
+}
+function max(a,b)
+{
+	if(a>b)
+		return a;
+	else
+		return b;
+}
+function min(a,b)
+{
+	if(a>b)
+		return b;
+	else
+		return a;
+}
+function shiftCipher(str)
+{
+	var key = 3;
+	var cip = "";
+	for(i=0;i<str.length();i++)
+	{
+		var t = findchar(str[i]);
+		cip += t;
+	}
+    doc
+}
